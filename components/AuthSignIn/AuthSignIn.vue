cb00bf19eb3b0d2a1d9ccb47899dcc32936590d1
aalduz <aldogeneral@gmail.com>
2024-08-23 02:15:23 +0200

    use hamifold logo in AuthSignIn component

--- components/AuthSignIn/AuthSignIn.vue
+++ components/AuthSignIn/AuthSignIn.vue
@@ -2,7 +2,7 @@
   <section class="mx-auto container py-40 px-[104px]">
     <div
       class="ham-shadow--active bg-white border border-gray-900 rounded-3xl flex flex-col justify-center items-center gap-10 p-16">
-      <Icon icon="ham-logo" :no-size="true" class="text-gray-900 h-[56px] w-[274px]" />
+      <Icon icon="hamifold-logo" :no-size="true" class="text-gray-900 h-[56px] w-[274px]" />
       <div class="flex flex-col items-center">
         <h2 class="text-display-sm uppercase font-display font-semibold pb-4">Welcome</h2>
         <p class="text-md text-gray-500">Create your own tokens and pages to claim them!</p>
