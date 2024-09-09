4e90c654dc8337e672a7249560639d1bd5fc5ab4
aalduz <aldogeneral@gmail.com>
2024-09-10 01:13:22 +0200

    add color variant for badge component appearance

--- components/Badge/Badge.vue
+++ components/Badge/Badge.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex items-center border w-fit py-1.5 px-3 uppercase font-semibold text-sm" :class="{
     'bg-white border border-gray-900 rounded-lg': color === 'primary',
+    'bg-gray-900 text-white border border-gray-900 rounded-lg': color === 'primary-invert',
     'bg-green-200 border border-green-500 text-green-500 rounded-lg': color === 'green',
     'bg-gray-900 text-white border border-gray-700 rounded-full': color === 'secondary',
     'text-xs': size === 'sm',
