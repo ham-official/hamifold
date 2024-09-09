cfe2475e22425f4395e69be9865f8a1a59a31a15
aalduz <aldogeneral@gmail.com>
2024-09-10 01:15:11 +0200

    rename TokensList to CardsList and make filters dynamic

---
+++ components/CardsList/CardsList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="bg-white border-2 border-gray-900 ham-shadow--active p-6 rounded-3xl text-gray-900 mt-6">
+    <h2 class="font-display text-display-sm uppercase font-semibold mb-4" :class="{
+      'text-center': noTokens,
+    }">{{ title }}</h2>
+    <template v-if="!noTokens">
+      <div class="flex justify-between items-center mb-4">
+        <ul class="flex justify-between gap-x-2">
+          <li v-for="filter in filters" :key="`filter-${filter}`">
+            <CTA :color="activeFilter === filter ? 'black' : 'gray'" @click="handleActiveFilter(filter)">{{ filter }}
+            </CTA>
+          </li>
+        </ul>
+      </div>
+      <ul class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <li v-for="(card, index) in visibleTokens" :key="`token-card-${index}`">
+          <Card class="ham-shadow cursor-pointer" v-bind="card" @click="$emit('click', { index })" />
+        </li>
+      </ul>
+    </template>
+    <p v-else class="text-center text-gray-400">You haven't minted any token yet</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cards: {
+      type: [Array, null],
+      default: null
+    },
+    title: {
+      type: String,
+      default: 'tokens'
+    },
+  },
+  data() {
+    return {
+      activeFilter: null,
+    }
+  },
+  computed: {
+    filters() {
+      return this.cards && new Set([...this.cards.map(c => c.type)])
+    },
+    visibleTokens() {
+      if (this.activeFilter) {
+        return this.cards && this.cards.filter(c => c.type === this.activeFilter)
+      }
+      return this.cards
+    },
+    noTokens() {
+      return !this.visibleTokens || this.visibleTokens.length === 0
+    }
+  },
+  methods: {
+    handleActiveFilter(filter) {
+      this.activeFilter = (this.activeFilter === filter) ? null : filter
+    }
+  },
+}
+</script>
