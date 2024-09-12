3f8323420309402320c5343a8e0d3f5e507de541
aalduz <aldogeneral@gmail.com>
2024-09-12 13:36:39 +0200

    remove ethers dependency and only import providers and Contract

--- utils/erc721Utils.js
+++ utils/erc721Utils.js
@@ -1,4 +1,4 @@
-import { ethers } from "ethers";
+import { providers, Contract } from "ethers";
 import wc from "@/api/platforms/walletConnect/setup.js";
 import { abi } from "@/data/abi/HamERC721";
 
@@ -6,14 +6,11 @@ export const safeMint = (uri, contractAddress) => {
   return new Promise(async (resolve, reject) => {
     try {
       const web3Provider = wc.modal.getWalletProvider();
-      const provider = new ethers.providers.Web3Provider(web3Provider);
-
+      const provider = new providers.Web3Provider(web3Provider);
       const signer = provider.getSigner();
-
-      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const contract = new Contract(contractAddress, abi, signer);
 
       const transaction = await contract.safeMint(uri);
-
       await transaction.wait();
 
       resolve(transaction);
