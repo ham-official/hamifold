63543a6ad3a333e497066582926f94bf22dca5b2
aalduz <aldogeneral@gmail.com>
2024-09-12 13:37:51 +0200

    remove space

--- components/PublishProgress/PublishProgress.vue
+++ components/PublishProgress/PublishProgress.vue
@@ -2,15 +2,15 @@
   <div class="flex flex-col gap-2 w-80">
     <ul v-if="steps" class="flex flex-col gap-4">
       <li v-for="s, index in steps" class="flex gap-4 py-4" :class="{
-      'border-b border-black': index < numberOfSteps - 1
-    }">
-        <Icon v-if="index === current" icon="refresh-cw-03" class=" animate-spin" />
+        'border-b border-black': index < numberOfSteps - 1
+      }">
+        <Icon v-if="index === current" icon="refresh-cw-03" class="animate-spin" />
         <Icon v-if="index < current" icon="check" class=" text-primary-700" />
         <span v-if="index > current" class="w-6 h-6"></span>
         <p class="font-semibold uppercase font-display" :class="{
-      'text-primary-700': index < current,
-      'text-gray-500': index > current
-    }">{{ s.action }}</p>
+          'text-primary-700': index < current,
+          'text-gray-500': index > current
+        }">{{ s.action }}</p>
       </li>
     </ul>
   </div>
